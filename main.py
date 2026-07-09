3ce78c3a21e8f678fdb8aaa0af2c9165d08564ad
hintzli-png <hintz.li@students.asdrp.org>
2026-07-09 15:24:05 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from wordslist import words_list
+import random
+
+word = list(random.choice(words_list))
+solved = ["_"]*len(word)
+guesses_left = 5
+guessed_letters = [] 
+
+while solved != word and guesses_left > 0:
+	print(" ".join(solved))
+	print(f"{guesses_left} guesses left \n")
+
+	guess = input("Guess a Letter \n")
+
+	if guess in guessed_letters or len(guess) > 1:
+		continue
+
+	guessed_letters.append(guess)
+
+	if guess in word:
+		for (i, letter) in enumerate(word):
+			if letter == guess:
+				solved[i] = letter
+
+	else:
+		guesses_left -= 1
+
+print(''.join(solved))
+
+if(solved == word):
+	print("YOU WIN!")
+else:
+  print(f"You did not get the word and now jimmy gets hurt the word was {''.join(word)}")
+  print("""
+    +---+
+    |   |
+    O   |
+   /|\\  |
+   / \\  |
+        |
+	=========""")
