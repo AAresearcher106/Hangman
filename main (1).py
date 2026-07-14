03834785992057ccc997daa03b2c3b1d16497fce
AAresearcher106 <aaron.horowitz@students.asdrp.org>
2026-07-14 14:18:21 -0700

    Tightening up to match rubric
    Fixed print statements to exactly match rubric, changed number of lives to match rubric, changed play again logic to require a yes or no answer as according to the rubric

--- main (1).py
+++ main (1).py
@@ -1,13 +1,13 @@
 from wordslist import words_list
 import random
 
-print("lets play hangman")
+print("Let's play hangman!")
 play_again = True
 
 while play_again:
         word = list(random.choice(words_list))
         solved = ["_"]*len(word)
-        guesses_left = len(word)
+        guesses_left = 6
         guessed_letters = [] 
         
         while solved != word and guesses_left > 0:
@@ -29,7 +29,7 @@ while play_again:
         
                 else:
                         guesses_left -= 1
-                        print("Letter not found in word")
+                        print("Letter not found!")
         
         print(''.join(solved))
         
@@ -47,7 +47,7 @@ while play_again:
                 else:
                         play_again = True
         else:
-                print(f"You did not get the word and now jimmy gets hurt the word was {''.join(word)}")
+                print(f"Out of lives, game over! The word was {''.join(word)}")
                 print("""
                           +---+
                           |   |
@@ -58,13 +58,20 @@ while play_again:
                         =========""")
 
                 print("")
-                        
-                choice = input("Game Over! Would you like to play again? (y/n) \n")
 
-                print("")
+                choice = 1
+                while choice != "no" and choice != "yes":
+                        choice = input("Play again? (yes/no) \n").lower()
 
-                if choice == "n":
-                        play_again = False
-                        print("Thank you for playing")
-                else:
-                        play_again = True
+                        print("")
+
+                        if choice == "no":
+                                play_again = False
+                                print("Thank you for playing")
+                                
+                        if choice == "yes":
+                                play_again = True
+
+                        else: 
+                                print("Invalid input.")
+                
